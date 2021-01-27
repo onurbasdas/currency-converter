1ad46e60342451162008b1c438e39c6430e0d34e
onurbasdas <onurebasdase@hotmail.com>
2021-01-27 13:16:34 +0300

    get url created

--- CurrencyConverter/ViewController.swift
+++ CurrencyConverter/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var chfLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var usdLabel: UILabel!
+    @IBOutlet weak var tryLabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func getRatesClicked(_ sender: Any) {
+        //        1- Request & Session
+        //        2- Response & Data
+        //        3- Parsing & JSON Serialization
+        
+        
+        //1.
+        let url = URL(string: "http://data.fixer.io/api/latest?access_key=01e970b74936e9ba92b6bd3b1b392341")
+        let session = URLSession.shared
+        //Closure
+        let task = session.dataTask(with: url!) { (data, response, error) in
+            if error != nil{
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+            }else{
+                //2.
+                if data != nil{
+                    
+                }
+            }
+        }
+        
+        
+        
+    }
+    
 }
 
